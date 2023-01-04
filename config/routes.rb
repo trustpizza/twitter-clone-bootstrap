e4de7c35ec27afc4a1909533500efab90af2eae2
trustpizza <maxelolsson@gmail.com>
2023-01-03 19:58:24 -0600

    All my efforts are for naught, turbo frames still do not work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :tweets, only: [:new, :index, :create]
+    resources :follows 
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :tweets, only: [:show, :edit, :update, :destroy] do
+    resources :likes
+    resources :comments 
+  end
+
+  resources :profiles
+
+  root 'tweets#index'
 end
