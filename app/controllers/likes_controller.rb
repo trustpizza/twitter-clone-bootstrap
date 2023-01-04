ad66e324ff7ef2cf5784712f291b4d3fa16daf06
trustpizza <maxelolsson@gmail.com>
2023-01-03 20:06:15 -0600

    BROKEN

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,26 +7,21 @@ class LikesController < ApplicationController
     @tweet = Tweet.find(params[:tweet_id])
     @like = @tweet.likes.create(like_params)
 
-    respond_to do |format| 
-      if @like.save
-        format.turbo_stream
-      else
-        format.html {render :new}
-      end
-    end 
+ 
   end
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     @like = @tweet.likes.find_by(like_params)
-
-    respond_to do |format|
-      if @like.destroy 
-        format.turbo_stream
-      else
-        format.html {render :new}
-      end
-    end
+    @like.destroy
+    
+    #respond_to do |format|
+     # if @like.destroy 
+      #  format.turbo_stream
+      #else
+      #  format.html {render :new}
+      #end
+    #end
   end
 
   private
